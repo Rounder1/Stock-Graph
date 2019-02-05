61914fbcadd1d0fe2532280b2aa9a0a34180f7d3
Rounder1 <tolerdavid@gmail.com>
2019-02-05 17:18:29 -0500

    added comments and made sure latest closing data is displayed

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,21 +1,24 @@
 $(document).ready(()=>{
 
+    // Creat Chart.js object and render empty chart to canvas.
     const ctx = $("#myChart");
     let chart = new Chart(ctx, {
             type: "line",
             data: {},
             options: {},
         });
-       
+    
+    // Get user input data and run API call and graph data.    
     $("#tickerGraphHistory").on("click",()=>{
         const userInput = $("#tickerInput").val();
         const userTicker = formatUserInput(userInput);
         getTickerHistory(userTicker);
     });
 
+    // format user input to remove spaces and make sure there are no junk characters. 
     function formatUserInput (userInput) {
-        const removeSpaces = userInput.replace(/\s/g,'');
         const alphaNumeric = /^[a-zA-Z0-9]*$/;
+        const removeSpaces = userInput.replace(/\s/g,'');
         if (!alphaNumeric.test(removeSpaces)) {
             return false;
         } else {
@@ -24,8 +27,9 @@ $(document).ready(()=>{
         }
     }
 
-    function graphTickerHistory(tickerDates, tickerPrice, tickerSymbol) {
-        const data = {
+    // Graph API data.
+    function graphTickerHistory(tickerDates, tickerPrice, tickerSymbol){
+        let data = {
             labels: tickerDates,
             datasets: [
                 {
@@ -39,7 +43,7 @@ $(document).ready(()=>{
                 }
             ]
         };
-        const options = {
+        let options = {
             title: {
                 display: true,
                 position: "top",
@@ -49,6 +53,7 @@ $(document).ready(()=>{
             }
         };
 
+        // Input new data into chart object.
         let chart = new Chart(ctx, {
             type: "line",
             data: data,
@@ -56,6 +61,7 @@ $(document).ready(()=>{
         });
     }
 
+    // API call to AlphaVantage. 
     function tickerHistoryPromise(tickerSymbol){
         return $.ajax({ 
                     url: "https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol="
@@ -65,8 +71,10 @@ $(document).ready(()=>{
     }
 
     function getTickerHistory(tickerSymbol){ 
+        // Make sure all promised data from tickerHistoryPromise API call was returned. 
         const promiseDone = $.when(tickerHistoryPromise(tickerSymbol));
         promiseDone.done((response)=>{
+            // Parse through returned AlphaVantage Object and push relevent data into an array so it is useable. 
             const monthlyData = [];
             const monthlyClosePrice = response["Monthly Time Series"];
             for (let dates in monthlyClosePrice) {
@@ -78,19 +86,28 @@ $(document).ready(()=>{
                     });
                 }
             }
+            // Create 2 new arrays that have stock closing prices for each year up to the last 20 years.
+            // Also make sure data is in the correct order.
             const graphDates = [];
-            for (let i = monthlyData.length-1; i >= 0; i-=12) {
+            for (let i = monthlyData.length-1; i >= 0; i-=12){
                 graphDates.push(monthlyData[i].date);
             }
             const graphPrice = [];
-            for (let i = monthlyData.length-1; i >= 0; i-=12) {
+            for (let i = monthlyData.length-1; i >= 0; i-=12){
                 graphPrice.push(monthlyData[i].close);
             }
-            if(graphDates.length < 1 || graphPrice.length < 1) {
+            // Make sure that the latest closing data is included in the graphDates and graphPrices arrays.
+            if (graphDates[graphDates.length-1] !== monthlyData[0].date
+                && graphPrice[graphPrice.length-1] !== monthlyData[0].close) {
+                    graphDates.push(monthlyData[0].date);
+                    graphPrice.push(monthlyData[0].close);
+                }
+            // Check to make sure that data is valid before graphing.
+            if(graphDates.length < 2 || graphPrice.length < 2) {
                 console.log("false");
             } else {
                 graphTickerHistory(graphDates, graphPrice, tickerSymbol);
-            }
+            }            
         });
     }
     
