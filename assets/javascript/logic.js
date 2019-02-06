3a9589a7f44d6a12af210a653dd57b080ff3b474
Rounder1 <tolerdavid@gmail.com>
2019-02-06 02:37:44 -0500

    fixed bugs and error messages

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,64 +1,56 @@
 $(document).ready(()=>{
 
     // Creat Chart.js object and render empty chart to canvas.
-    const ctx = $("#myChart");
-    let chart = new Chart(ctx, {
+    const ctx = $("#chartCanvas");
+    const chart = new Chart(ctx, {
             type: "line",
             data: {},
-            options: {},
+            options: {
+                title: {
+                    display: true,
+                    position: "top",
+                    text: "Stock Graph",
+                    fontSize: 14,
+                    fontColor: "#111"
+                }
+            }
         });
     
-    // Get user input data and run API call and graph data.    
+    // Get user input data from textbox and format it.
     $("#tickerGraphHistory").on("click",()=>{
         const userInput = $("#tickerInput").val();
         const userTicker = formatUserInput(userInput);
+        // clear error messages
+        $("#errorContainer").empty();
         getTickerHistory(userTicker);
     });
 
-    // format user input to remove spaces and make sure there are no junk characters. 
+    // Format user input to remove spaces and uppercase letters.
     function formatUserInput (userInput) {
-        const alphaNumeric = /^[a-zA-Z0-9]*$/;
         const removeSpaces = userInput.replace(/\s/g,'');
-        if (!alphaNumeric.test(removeSpaces)) {
-            return false;
-        } else {
-            const formatingDone = removeSpaces.toUpperCase();
-            return formatingDone;
-        }
+        const formatingDone = removeSpaces.toUpperCase();
+        return formatingDone;
     }
 
-    // Graph API data.
-    function graphTickerHistory(tickerDates, tickerPrice, tickerSymbol){
-        let data = {
-            labels: tickerDates,
-            datasets: [
-                {
-                    label: tickerSymbol,
-                    data: tickerPrice,
-                    backgroundColor: "green",
-                    borderColor: "lightgreen",
-                    fill: false,
-                    lineTension: 0,
-                    pointRadius: 5
-                }
-            ]
-        };
-        let options = {
-            title: {
-                display: true,
-                position: "top",
-                text: "Stock Graph",
-                fontSize: 14,
-                fontColor: "#111"
-            }
-        };
+    // handle writing user friendly error messages.
+    function handleErrorMessage(desiredErrorMessage) {
+        const errorText = $("<div class='alert alert-danger' role='alert'> </div>").text(desiredErrorMessage);
+        $("#errorContainer").append(errorText);
+    }
 
-        // Input new data into chart object.
-        let chart = new Chart(ctx, {
-            type: "line",
-            data: data,
-            options: options
-        });
+    // Update graph object with processed API data.
+    function graphTickerHistory(tickerDates, tickerPrice, tickerSymbol){
+        chart.config.data.labels = tickerDates;
+        chart.config.data.datasets = [{
+                label: tickerSymbol,
+                data: tickerPrice,
+                backgroundColor: "green",
+                borderColor: "lightgreen",
+                fill: false,
+                lineTension: 0,
+                pointRadius: 5
+            }];
+        chart.update();
     }
 
     // API call to AlphaVantage. 
@@ -73,7 +65,24 @@ $(document).ready(()=>{
     function getTickerHistory(tickerSymbol){ 
         // Make sure all promised data from tickerHistoryPromise API call was returned. 
         const promiseDone = $.when(tickerHistoryPromise(tickerSymbol));
+        // Handle error if API call fails.
+        promiseDone.fail(()=>{
+            handleErrorMessage("Error... There was an issue connecting to the securities data. \
+            You may have to try again another time.");
+        });
+        // Handle successful API call.
         promiseDone.done((response)=>{
+            // Handle error if API does not recognize the ticker OR if user queries too quickly for API.
+            console.log(response);
+            if (response["Error Message"]){
+                handleErrorMessage("Error... Information for the ticker you entered is not available. \
+                Please make sure the symbol is correct.");
+                return;
+            } else if (response["Note"]){
+                handleErrorMessage("Error... Apologies you have performed too many queries at one time. \
+                Our access the securities data is a bit limited. Please wait a few moments before \
+                searching again");
+            }
             // Parse through returned AlphaVantage Object and push relevent data into an array so it is useable. 
             const monthlyData = [];
             const monthlyClosePrice = response["Monthly Time Series"];
@@ -86,7 +95,7 @@ $(document).ready(()=>{
                     });
                 }
             }
-            // Create 2 new arrays that have stock closing prices for each year up to the last 20 years.
+            // Create two new arrays that have stock closing prices for each year up to the last 20 years.
             // Also make sure data is in the correct order.
             const graphDates = [];
             for (let i = monthlyData.length-1; i >= 0; i-=12){
@@ -101,13 +110,8 @@ $(document).ready(()=>{
                 && graphPrice[graphPrice.length-1] !== monthlyData[0].close) {
                     graphDates.push(monthlyData[0].date);
                     graphPrice.push(monthlyData[0].close);
-                }
-            // Check to make sure that data is valid before graphing.
-            if(graphDates.length < 2 || graphPrice.length < 2) {
-                console.log("false");
-            } else {
-                graphTickerHistory(graphDates, graphPrice, tickerSymbol);
-            }            
+                } 
+            graphTickerHistory(graphDates, graphPrice, tickerSymbol);             
         });
     }
     
